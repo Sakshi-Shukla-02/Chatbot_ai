fe47d24c21b3b8fe6d4f516afb92b4a47d6f3469
Sakshi Shukla <sakshishukla271102@gmail.com>
2025-05-13 20:42:44 +0530

    added footer,body and header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,36 @@ const App = () =>
 {
   return(<div className="container">
   <div className="chatbot-popup">
+    {/* Chatbot Header */}
     <div className = "chat-header">
     <div className="header-info">
      <ChatbotIcon/>
     <h2 className="logo-text">Chatbot</h2>
     </div>
+    <button className="material-symbols-outlined">
+    keyboard_arrow_down
+    </button>
+    </div>
+    {/* Chatbot Body */}
+    <div className="chat-body">
+    <div className="message bot-message">
+      <ChatbotIcon/>
+      <p className="message-text">
+        Hey There 👋 <br/> How can I help you today? 
+      </p>
+    </div>
+    <div className="message user-message">
+      <p className="message-text">
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. At numquam similique inventore nisi.
+      </p>
+    </div>
+    </div>
+    {/* Chatbot Footer */}
+    <div className="chat-footer">
+      <form action="#" className="chat-form">
+        <input type="text" placeholder = "Message....." className="message-input" required/>
+      </form>
+
     </div>
   </div>
   </div>
